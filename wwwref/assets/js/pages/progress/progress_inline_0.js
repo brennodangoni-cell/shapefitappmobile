52039c066c762ed14ac71c60a86c67769f9f606e
semtetog <brennodmoraes@gmail.com>
2025-11-27 19:12:43 -0300

    chore: remover arquivos desnecessários do repositório
    - Remove PLUGIN_OPTIONS.md
    - Remove check-mlkit-setup.md
    - Remove ajax_search_foods_recipes.php
    - Remove get_add_food_data.php
    - Remove serve.js
    - Remove pasta wwwref/
    - Adiciona todos ao .gitignore

--- wwwref/assets/js/pages/progress/progress_inline_0.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Script Inline Protegido - inline_0
- * Envolvido em IIFE para evitar conflitos de variáveis globais.
- */
-(function() {
-    // NÃO sobrescrever BASE_APP_URL - config.js já define corretamente
-    // Isso quebra no Capacitor onde pathname não funciona igual
-})();
--- wwwref/assets/js/pages/progress/progress_inline_1.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * Script Inline Protegido - inline_1
- * Envolvido em IIFE para evitar conflitos de variáveis globais.
- */
-(function() {
-    // Bottom nav já é carregado pelo router/index.html - não precisa carregar aqui
-})();
